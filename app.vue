39574003d5b34f75c70b02f8043b3fd27caf628d
Evren Ceyhan <evren@ceyhan.io>
2022-04-25 22:23:53 +0300

    bootstrap added

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container py-5">
+
+    <h1 class="display-4">RestoAdvisor</h1>
+
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,22 @@
-import { defineNuxtConfig } from 'nuxt'
+import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+    app: {
+        head: {
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: '/bootstrap.min.css',
+                    // href: '/node_modules/bootstrap/dist/css/bootstrap.min.css',
+                },
+            ],
+            script: [
+                {
+                    src: '/bootstrap.bundle.min.js',
+                    // src: '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+                },
+            ],
+        },
+    },
+});

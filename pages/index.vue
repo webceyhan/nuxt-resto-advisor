55da0cfc574fcb2188e0e35783f9fdbf395c3631
Evren Ceyhan <evren@ceyhan.io>
2022-04-26 10:58:04 +0300

    restaurants page added

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <Navbar />
+    <Navbar />
 
-  <main class="container text-center py-5">
-    <h1 class="display-4">Welcome to RestoAdvisor</h1>
-  </main>
+    <main class="container text-center py-5">
+        <h1 class="display-4">Welcome to RestoAdvisor</h1>
+
+        <a href="/restaurants" class="btn btn-primary"> View Restaurants </a>
+    </main>
 </template>
---
+++ pages/restaurants.vue
@@ -0,0 +1,7 @@
+<template>
+    <Navbar />
+
+    <main class="container text-center py-5">
+        <h1 class="display-4">Hello this is the restaurants</h1>
+    </main>
+</template>

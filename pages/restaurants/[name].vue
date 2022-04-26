8fd081f1d79665acc7b0ef73d6bef93ae20c22fb
Evren Ceyhan <evren@ceyhan.io>
2022-04-26 13:45:43 +0300

    restaurant-detail page impl.

--- pages/restaurants/[name].vue
+++ pages/restaurants/[name].vue
@@ -6,6 +6,32 @@ const { selected } = useRestaurants();
   <Navbar />
 
   <main class="container py-5">
-    <h1 class="display-5 mb-4">{{ selected.name }}</h1>
+    <div class="row">
+      <!-- image -->
+      <section class="col">
+        <img class="img-fluid" :src="selected.imageUrl" alt="restaurant image" />
+      </section>
+
+      <!-- info -->
+      <section class="col-5">
+        <h1 class="display-5">{{ selected.name }}</h1>
+
+        <hr class="mb-4" />
+
+        <h5 class="d-flex justify-content-between mb-4">
+          Revenue (in billions)
+          <span class="badge bg-dark"> {{ selected.revenue }} </span>
+        </h5>
+
+        <h5 class="d-flex justify-content-between mb-4">
+          Number of Stores
+          <span class="badge bg-dark">
+            {{ selected.numberOfStores }}
+          </span>
+        </h5>
+
+        <p class="lead">{{ selected.content }}</p>
+      </section>
+    </div>
   </main>
 </template>

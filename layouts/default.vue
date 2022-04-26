315c2f1bd3fefd817f9d3db5c9e6634b8a60ec82
Evren Ceyhan <evren@ceyhan.io>
2022-04-26 17:53:14 +0300

    meta properties added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
+        <Html lang="en-US">
+            <Head>
+                <Title>RestoAdvisor</Title>
+                <Meta
+                    name="description"
+                    content="A great Restaurant Advisor"
+                ></Meta>
+                <Link rel="icon" href="/favicon.png"></Link>
+            </Head>
+        </Html>
+
         <Navbar />
 
         <main class="container py-5">

f72d1cd557d5c783c492c30130f5a63eb2541ffa
Evren Ceyhan <evren@ceyhan.io>
2022-04-26 10:28:05 +0300

    navbar added

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
+  <Navbar />
+
   <div class="container py-5">
-
     <h1 class="display-4">RestoAdvisor</h1>
-
   </div>
 </template>
---
+++ components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar navbar-expand navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">RestoAdvisor</a>
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/restaurants">Restaurants</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>

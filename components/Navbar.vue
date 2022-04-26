bd75a68071b5f2ce281e80514ff2c67e19480ec5
Evren Ceyhan <evren@ceyhan.io>
2022-04-26 11:54:46 +0300

    navbar container set to grid

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand navbar-dark bg-dark">
-    <div class="container-fluid">
+    <div class="container">
       <a class="navbar-brand" href="/">RestoAdvisor</a>
 
       <button

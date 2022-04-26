06aed532367b7a244ae9049e4ab639af2190a965
Evren Ceyhan <evren@ceyhan.io>
2022-04-26 11:23:36 +0300

    restaurant data added to populate tables

--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+import data from "../data.json";
+
+const cols = computed(() => {
+  const count = data.length;
+  const half = Math.floor(count / 2);
+
+  return {
+    first: data.slice(0, half),
+    second: data.slice(half, count),
+  };
+});
+</script>
+
 <template>
   <Navbar />
 
@@ -7,13 +21,13 @@
     <div class="row row-cols-1 row-cols-md-2 g-4">
       <div class="col">
         <RestaurantTable>
-          <RestaurantRow :rank="1" :name="`hello`" />
+          <RestaurantRow v-for="resto in cols.first" :key="resto.id" v-bind="resto" />
         </RestaurantTable>
       </div>
 
       <div class="col">
         <RestaurantTable>
-          <RestaurantRow :rank="1" :name="`hello`" />
+          <RestaurantRow v-for="resto in cols.second" :key="resto.id" v-bind="resto" />
         </RestaurantTable>
       </div>
     </div>

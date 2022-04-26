d39da03a032a4f92e6122762da8771a1ae07e078
Evren Ceyhan <evren@ceyhan.io>
2022-04-26 11:24:40 +0300

    text-centerin limited to head

--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -15,8 +15,8 @@ const cols = computed(() => {
 <template>
   <Navbar />
 
-  <main class="container text-center py-5">
-    <h1 class="display-4 mb-4">Hello this is the restaurants</h1>
+  <main class="container py-5">
+    <h1 class="display-4 text-center mb-4">Hello this is the restaurants</h1>
 
     <div class="row row-cols-1 row-cols-md-2 g-4">
       <div class="col">

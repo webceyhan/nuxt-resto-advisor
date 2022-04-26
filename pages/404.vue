e5ce2c876242ace590b1ff08cb2aa116a79d7a70
Evren Ceyhan <evren@ceyhan.io>
2022-04-26 17:07:06 +0300

    404 sad-face added

--- pages/404.vue
+++ pages/404.vue
@@ -2,6 +2,13 @@
     <div class="text-center">
         <h1 class="display-1">Page not found!</h1>
 
+        <img
+        class="img-fluid" style="max-width: 200px;"
+            src="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+        />
+
+        <br>
+
         <NuxtLink to="/restaurants">Go to Restaurants</NuxtLink>
     </div>
 </template>

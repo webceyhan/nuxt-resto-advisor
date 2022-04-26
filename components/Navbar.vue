06b6e2c9db56fc0083c1c2a75cae6aa0d87fc2f6
Evren Ceyhan <evren@ceyhan.io>
2022-04-26 15:06:23 +0300

    active class removed in nav link

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -15,9 +15,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <NuxtLink class="nav-link active" to="/"
-                            >Home
-                        </NuxtLink>
+                        <NuxtLink class="nav-link" to="/">Home </NuxtLink>
                     </li>
                     <li class="nav-item">
                         <NuxtLink class="nav-link" to="/restaurants"
